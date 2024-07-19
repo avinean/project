f01c537b07be9a6e9182b7d85793ba2ea1f6b0da
Vladyslav Lubeznyy <Vladyslav_Lubeznyy@vfc.com>
2024-07-19 21:27:11 +0300

    test change

--- ui/apps/landing/components/product/carousel.vue
+++ ui/apps/landing/components/product/carousel.vue
@@ -5,14 +5,19 @@ const { data } = await findPreview()
 </script>
 
 <template>
-  <UCarousel
-  v-slot="{ item }" 
-  :items="data?.data" arrows indicators 
-  :ui="{  item: 'basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4' }"
-  class="gap-2">
-  <div class="p-2 flex">
-
-    <ProductCard v-bind="item.attributes" />
+  <div>
+    <UCarousel
+      v-if="data?.data"
+      v-slot="{ item }" 
+      :items="data?.data"
+      arrows
+      indicators 
+      :ui="{  item: 'basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4' }"
+      class="gap-2"
+    >
+      <div class="p-2 flex">
+        <ProductCard v-bind="item.attributes" />
+      </div>
+    </UCarousel>
   </div>
-  </UCarousel>
 </template>
